23f4b0cd5cf8598be9949922d10f17c4a2d53c1d
m3122 <3122mackenzie@gmail.com>
2023-03-29 19:48:15 -0300

    Added more css features

--- src/componentes/Form.js
+++ src/componentes/Form.js
@@ -5,6 +5,7 @@ import './Form.css';
 export const Form = () => {
   const [name, set_name] = useState('');
   const [rol, set_rol] = useState('');
+  const [error1, set_error1] = useState(false);
 
   const handle_rol = event => {
     let value = event.target.value.slice(0, 11).toLowerCase()
@@ -13,20 +14,22 @@ export const Form = () => {
     if (isNaN(usm_id))
       return
     if (value.length > 9) {
-      if (value[9] != '-')
+      if (value[9] !== '-')
         value = usm_id + '-' + value.slice(9)
-      if (value.length > 10 && isNaN(value[10]) && value[10] != 'k')
+      if (value.length > 10 && isNaN(value[10]) && value[10] !== 'k')
         return
     }
     set_rol(value)
   }
 
   const rol_verification = _ => {
-    if (rol.length != 11) {
-      // Hacer que te diga que está malo
-      return
+    if (rol.length !== 11) {
+      set_error1(true)
     }
-    let formatted_rol = rol.slice(0, 9) + (rol[10] == 'k'? 0: rol[10])
+    else{
+      set_error1(false)
+    }
+    let formatted_rol = rol.slice(0, 9) + (rol[10] === 'k'? 0: rol[10])
     console.log(formatted_rol)
   }
 
@@ -34,25 +37,26 @@ export const Form = () => {
     <div className='Form'>
       <br />
       <label>
-        ROL: <input onChange={handle_rol} onBlur={rol_verification} value={rol}/>
+        <b>ROL</b><br /><input placeholder="123456789-k" onChange={handle_rol} onBlur={rol_verification} value={rol}/>
+        {error1 && (<div style={{color: '#e03434'}}>Rol Invalido</div>)}
       </label>
       <br />
       <label>
-        Nombre Completo: <input onChange={event => set_name(event.target.value)} value={name}/>
+        <b>Nombre completo</b><br /><input placeholder="Luis Apellido" onChange={event => set_name(event.target.value)} value={name}/>
       </label>
       <br />
       <label>
-        Apodo: <input defaultValue={name.split(' ')[0]}/>
+        <b>Apodo</b><br /><input placeholder="Lucho" defaultValue={name.split(' ')[0]}/>
       </label>
       <div>
-        <u>Nota</u>: Elije un apodo fácilmente identificable por todos tus compañeros;
+        <u>Nota</u>: Elige un apodo fácilmente identificable por todos tus compañeros;
         tu primer, segundo, ámbos nombres o tu nombre social son una buena opción, <b>evita usar tu Gamer Tag</b> o
         cualquier apodo que sea difícil de relacionar contigo para alguien que no te conoce de primera mano.
         <ul>
           <li>Felipe Rojas → Pipe ✔️</li>
           <li>Felipe Rojas → pyeom ❌</li>
           <li>Vicente Mackenzie → Makenki ✔️</li>
-          <li>Vicente Mackenzie → E122 ❌</li>
+          <li>Vicente Mackenzie → 3122 ❌</li>
         </ul>
       </div>
     </div>
