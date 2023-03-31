60efb58a94c4974b34de86e3afcfd1b5c11235dc
Lonkk <aiday33@hotmail.com>
2023-03-30 21:53:12 -0300

    change input

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <div className="App">
       <div className='Container'>
-        <h1 style={{color:'aliceblue'}}>Ingrese sus datos:</h1>
         <Form className="Form"/>
       </div>
     </div>
