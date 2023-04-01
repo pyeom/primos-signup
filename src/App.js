a6954605d2985df1faf10d1e3b957b78aee561b5
lilkimo <zarkokuljis.zk@gmail.com>
2023-04-01 16:20:26 -0300

    implemented msal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import './App.css'
 import {Form} from './componentes/Form'
+import {useEffect, useState} from 'react'
 
-export default function App() {
+// MSAL imports
+import {MsalProvider} from '@azure/msal-react'
+import {useMsal} from '@azure/msal-react'
+import {InteractionStatus, InteractionRequiredAuthError} from '@azure/msal-browser'
+import {loginRequest} from './authConfig'
+
+import {callMsGraph} from './utils/MsGraphApiCall'
+import { Schedule } from './componentes/Schedule'
+
+export default function App({pca}) {
+  const {instance, inProgress} = useMsal()
+  const [graphData, setGraphData] = useState(null)
+
+  useEffect(() => {
+    if (!graphData && inProgress === InteractionStatus.None) {
+      callMsGraph()
+        .then(response => setGraphData(response))
+        .catch(e => {
+          if (e instanceof InteractionRequiredAuthError) {
+            instance.acquireTokenRedirect({
+              ...loginRequest,
+              account: instance.getActiveAccount(),
+            })
+          }
+        })
+    }
+  }, [inProgress, graphData, instance])
+
   return (
-    <div className='App'>
-      <div className='Container'>
-        <Form className='Form' />
-      </div>
-    </div>
+    <MsalProvider instance={pca}>
+      {graphData && (
+        <div className='App'>
+          <div className='Container'>
+            <Form />
+          </div>
+        </div>
+      )}
+    </MsalProvider>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,41 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+// MSAL shit
+import {PublicClientApplication, EventType} from '@azure/msal-browser'
+import {msalConfig, loginRequest} from './authConfig'
+
+export const msalInstance = new PublicClientApplication(msalConfig)
+
+// Activar la cuenta si hay alguna ya registrada cuando se inicia la página
+const accounts = msalInstance.getAllAccounts()
+if (accounts.length > 0) {
+  if (accounts.length > 1) console.warn(`Hay ${accounts.length} cuentas logeadas`)
+  if (msalInstance.getActiveAccount() == null) {
+    console.log(`Ninguna cuenta activa, iniciando sesión con ${accounts[0]}`)
+    msalInstance.setActiveAccount(accounts[0])
+  }
+}
+
+msalInstance.addEventCallback(event => {
+  // Activar la cuenta cuando se logee
+  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
+    const account = event.payload.account
+    msalInstance.setActiveAccount(account)
+  }
+  // Solicitar logeo cuando cierras la cuenta
+  else if (
+    event.eventType === EventType.HANDLE_REDIRECT_END &&
+    msalInstance.getActiveAccount() == null
+  )
+    msalInstance.loginRedirect(loginRequest)
+})
+// END of MSAL shit
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <App pca={msalInstance} />
   </React.StrictMode>
 )
 
