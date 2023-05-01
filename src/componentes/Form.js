63fde04a3be8f7cbcd7d1ac46e23cc47bd59b0d3
lilkimo <zarkokuljis.zk@gmail.com>
2023-05-01 16:03:29 -0400

    From improves

--- src/componentes/Form.js
+++ src/componentes/Form.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 import './Form.css'
 
-export const Form = ({fullname}) => {
+export const Form = ({fullname, mail, is_schedule, set_schedule}) => {
   const [name, set_name] = useState(fullname.toLowerCase())
-  const [rol, set_rol] = useState('')
-  const [schedule, set_schedule] = useState(null)
   const [error_1, set_error_1] = useState(false)
+  const [rol, set_rol] = useState('')
   const [error_2, set_error_2] = useState(false)
+  const siga_password = useRef(null)
+  const [disable_button, set_disable_button] = useState(false)
+  const [error_3, set_error_3] = useState(false)
 
   const handle_rol = event => {
     let value = event.target.value.slice(0, 11).toLowerCase()
@@ -26,6 +28,27 @@ export const Form = ({fullname}) => {
     let formatted_rol = rol.slice(0, 9) + (rol[10] === 'k' ? 0 : rol[10])
   }
 
+  async function get_schedule() {
+    set_disable_button(true)
+    const request_options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        login: mail[0],
+        server: mail[1],
+        passwd: siga_password.current.value,
+      }),
+    }
+    await fetch('http://127.0.0.1:8000/api/schedule', request_options).then(response => {
+      if (response.ok) response.json().then(r => set_schedule(r.schedule))
+      else set_error_3(true)
+    })
+    set_disable_button(false)
+  }
+
   return (
     <div className='Form gapped'>
       <h1>
@@ -40,25 +63,25 @@ export const Form = ({fullname}) => {
             onChange={event => set_name(event.target.value)}
             value={name}
           />
-          <label class='input_label'>
+          <label className='input_label'>
             <b>Nombre Completo</b>
           </label>
         </div>
-        <div className='gapped'>
-          <input
-            type='text'
-            className='box input_field'
-            placeholder='123456789-k'
-            onChange={handle_rol}
-            onBlur={rol_verification}
-            value={rol}
-          />
-          <label class='input_label'>
-            <b>Rol</b>
-          </label>
-        </div>
-        {error_1 && <div className='box info error'>Rol Inválido</div>}
       </div>
+      <div className='gapped'>
+        <input
+          type='text'
+          className='box input_field'
+          placeholder='123456789-k'
+          onChange={handle_rol}
+          onBlur={rol_verification}
+          value={rol}
+        />
+        <label className='input_label'>
+          <b>Rol</b>
+        </label>
+      </div>
+      {error_1 && <div className='box info error'>Rol Inválido</div>}
       <div className='gapped'>
         <div>
           <input
@@ -73,7 +96,7 @@ export const Form = ({fullname}) => {
           </label>
         </div>
       </div>
-      {error_2 && (
+      {(!is_schedule && error_2) && (
         <div className='box info error'>
           <u>Nota</u>: Elige un apodo fácilmente identificable por todos tus compañeros; tu primer,
           segundo, ámbos nombres o tu nombre social son una buena opción.&ensp;
@@ -98,14 +121,17 @@ export const Form = ({fullname}) => {
       <div className='gapped'>
         <div>
           <input
-            disabled={schedule}
-            type='password'
+            ref={siga_password}
             className='box input_field capitalize'
+            type='password'
+            disabled={is_schedule}
           />
           <label className='input_label'>
             <b>Clave del SIGA</b>
           </label>
         </div>
+      </div>
+      {(!error_3 || is_schedule) && (
         <div className='box info'>
           Tus credenciales de acceso al SIGA serán utilizadas exclusivamente para obtener tu horario
           y no serán almacenadas de ninguna forma. Recuerda que este proyecto es open source, por lo
@@ -113,25 +139,23 @@ export const Form = ({fullname}) => {
           <a href='https://github.com/m3122/primos-signup'>Front-end</a>
           <a href='https://github.com/lilkimo/primos-signup-backend'>Back-end</a>
         </div>
-      </div>
-      {!schedule ? (
-        <div className='button-container'>
-          <button
-            className='box submit'
-            type='button'
-            onClick={_ => set_schedule({})}
-          >
-            <b>Importar Horario</b>
-          </button>
-        </div>
+      )}
+      {!is_schedule ? (
+        <>
+          {error_3 && <div className='box info error'>Clave incorrecta</div>}
+          <div className='button-container'>
+            <button
+              className='box'
+              type='button'
+              disabled={disable_button}
+              onClick={_ => get_schedule()}
+            >
+              <b>Importar Horario</b>
+            </button>
+          </div>
+        </>
       ) : (
         <div className='button-container'>
-          <button
-            className='box cancel'
-            type='button'
-          >
-            <b>Cancelar</b>
-          </button>
           <button
             className='box submit'
             type='button'
